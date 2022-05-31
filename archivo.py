41aaeb0ac9cec836ad1a3e3c1299d169b34fe37a
davidcubillos0211 <100205678+davidcubillos0211@users.noreply.github.com>
2022-05-31 13:38:23 +0200

    Add files via upload

---
+++ archivo.py
@@ -0,0 +1,28 @@
+#cadenas 6
+s="ma123454ñana a las 10 o a las 11"
+def num(str):
+    posicion=0
+    contador=0
+    for i in str:
+            
+        if i.isnumeric():
+            
+            inicio=contador
+            
+            for i in (str[contador:]):
+                
+                contador+=1
+                if i.isnumeric():
+                    final=contador
+                else:
+                    primernumero= int(str[inicio:final])
+                    
+                    return primernumero
+                if contador==len(str):
+                    primernumero= int(str[inicio:final])
+                    
+                    return primernumero
+                    
+        contador+=1
+
+print(num(s))
